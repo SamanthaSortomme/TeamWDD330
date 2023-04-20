4c6eef252dbfd00375ac79d07f0f5b9ce813f1e4
SamanthaSortomme <sor23001@byui.edu>
2023-04-19 18:16:30 -0700

    w01 done

---
+++ src/js/product.js
@@ -0,0 +1,31 @@
+import { setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+
+const dataSource = new ProductData("tents");
+
+function addProductToCart(product) {
+  // setLocalStorage("so-cart", product);
+  let currentCart = JSON.parse(localStorage.getItem("so-cart"));
+  if (!currentCart){
+    currentCart = [];
+  }
+    currentCart.push(product);
+    setLocalStorage("so-cart", currentCart);
+
+  }
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
+
+
+// let currentCart = JSON.parse(localStorage.getItem("so-cart"));
+// currentCart.push(product);
+// setLocalStorage("so-cart", currentCart);
